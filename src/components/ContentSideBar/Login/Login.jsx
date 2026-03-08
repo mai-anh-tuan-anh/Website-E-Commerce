547a849e4b6165e39236de36b1426b1a44e35bf0
MATA <sktt10mtd@gmail.com>
2026-03-08 15:34:56 +0700

    finish ui/ux login sidebar

--- src/components/ContentSideBar/Login/Login.jsx
+++ src/components/ContentSideBar/Login/Login.jsx
@@ -1,18 +1,61 @@
 import InputCommon from '@components/InputCommon/InputCommon';
 import styles from './styles.module.scss';
 import Button from '@components/Button/Button';
+import { FaFacebookF, FaTwitter, FaGoogle } from 'react-icons/fa';
+
 function Login() {
-    const { container, title, rememberMe } = styles;
+    const {
+        container,
+        title,
+        formGroup,
+        signInButton,
+        divider,
+        socialLogin,
+        socialButtons,
+        socialButton,
+        facebookButton,
+        twitterButton,
+        googleButton,
+        footerLinks,
+        signupLink,
+        forgotPasswordLink
+    } = styles;
+
     return (
         <div className={container}>
-            <div className={title}>SIGN IN</div>
-            <InputCommon label='Username' type='text' isRequired />
-            <InputCommon label='Password' type='password' isRequired />
-            <div className={rememberMe}>
-                <input type='checkbox' />
-                <span>Remember me</span>
+            <div className={title}>LOG IN</div>
+
+            <div className={formGroup}>
+                <InputCommon label='Email' type='email' isRequired />
+                <InputCommon label='Password' type='password' isRequired />
             </div>
-            <Button content='Sign In' />
+
+            <div className={signInButton}>
+                <Button content='Sign In' />
+            </div>
+
+            <div className={divider}>or</div>
+
+            <div className={socialLogin}>
+                <div className={socialButtons}>
+                    <button className={`${socialButton} ${facebookButton}`}>
+                        <FaFacebookF />
+                    </button>
+                    <button className={`${socialButton} ${twitterButton}`}>
+                        <FaTwitter />
+                    </button>
+                    <button className={`${socialButton} ${googleButton}`}>
+                        <FaGoogle />
+                    </button>
+                </div>
+            </div>
+
+            <div className={footerLinks}>
+                <span className={signupLink}>Signup</span>
+                <span className={forgotPasswordLink}>
+                    Forgot your password?
+                </span>
+            </div>
         </div>
     );
 }
