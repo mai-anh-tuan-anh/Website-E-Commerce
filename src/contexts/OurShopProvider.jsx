fa35e64e9eff9a8a35ade3b769a1f830382c7f02
MATA <sktt10mtd@gmail.com>
2026-03-17 13:43:55 +0700

    test

--- src/contexts/OurShopProvider.jsx
+++ src/contexts/OurShopProvider.jsx
@@ -1,5 +1,5 @@
 import { createContext, useEffect, useState } from 'react';
-import { getProducts } from '../apis/productsService';
+import { getProducts } from '@/apis/productsService';
 export const OurShopContext = createContext();
 export const OurShopProvider = ({ children }) => {
     const sortOptions = [
